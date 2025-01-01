4ca05a40eaca3c62a195d5b8ceb740516be48045
Daniel Giathi <blacksharkchi@proton.me>
2025-01-01 16:49:29 +0300

    install axios and heroicons for api requests and icons respectively

--- the-health-hero-frontend/src/index.js
+++ the-health-hero-frontend/src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render (
-    <App />
-)
+root.render (<App />)
